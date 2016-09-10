c55a7c2efeb8e3361db53d4e9b9806c9f432b9e2
Bob Whitelock <bob.whitelock1@gmail.com>
2016-09-10 11:43:38 +0100

    Add age to form tutorial

--- examples/3-form.elm
+++ examples/3-form.elm
@@ -4,7 +4,6 @@ import Html.Attributes exposing (..)
 import Html.Events exposing (onInput)
 import String exposing (length)
 
-
 main =
   Html.beginnerProgram
     { model = model
@@ -21,12 +20,13 @@ type alias Model =
   { name : String
   , password : String
   , passwordAgain : String
+  , age : String
   }
 
 
 model : Model
 model =
-  Model "" "" ""
+  Model "" "" "" ""
 
 
 
@@ -37,7 +37,7 @@ type Msg
     = Name String
     | Password String
     | PasswordAgain String
-
+    | Age String
 
 update : Msg -> Model -> Model
 update msg model =
@@ -51,6 +51,9 @@ update msg model =
     PasswordAgain password ->
       { model | passwordAgain = password }
 
+    Age age ->
+      { model | age = age }
+
 
 
 -- VIEW
@@ -62,6 +65,7 @@ view model =
     [ input [ type' "text", placeholder "Name", onInput Name ] []
     , input [ type' "password", placeholder "Password", onInput Password ] []
     , input [ type' "password", placeholder "Re-enter Password", onInput PasswordAgain ] []
+    , input [ type' "number", placeholder "Age", onInput Age ] []
     , viewValidation model
     ]
 
@@ -74,6 +78,8 @@ viewValidation model =
         error  "Passwords do not match!"
       else if length model.password < 8 then
         error  "Password too short"
+      else if invalidAge model.age then
+        error "You can't be that old!"
       else
         ok
   in
@@ -86,3 +92,11 @@ error string =
 ok : (String, String)
 ok =
   ("green", "OK")
+
+invalidAge : String -> Bool
+invalidAge age =
+  case String.toInt age of
+    Ok int ->
+      int < 0
+    Err _ ->
+      True
