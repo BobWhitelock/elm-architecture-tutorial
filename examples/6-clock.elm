f2e0ed22579dfcfd5e72f0b14fcd17435cebe909
Bob Whitelock <bob.whitelock1@gmail.com>
2016-09-12 22:32:07 +0100

    Add stop/start button to clock example

--- examples/6-clock.elm
+++ examples/6-clock.elm
@@ -1,5 +1,6 @@
-import Html exposing (Html)
+import Html exposing (Html, div, button)
 import Html.App as Html
+import Html.Events exposing (onClick)
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
 import Time exposing (Time, second)
@@ -19,12 +20,15 @@ main =
 -- MODEL
 
 
-type alias Model = Time
+type alias Model = {
+  time : Time,
+  running : Bool
+}
 
 
 init : (Model, Cmd Msg)
 init =
-  (0, Cmd.none)
+  (Model 0 True, Cmd.none)
 
 
 
@@ -33,13 +37,16 @@ init =
 
 type Msg
   = Tick Time
+  | ToggleRunning
 
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
     Tick newTime ->
-      (newTime, Cmd.none)
+      ({model | time = newTime}, Cmd.none)
+    ToggleRunning ->
+      ({model | running = (not model.running)}, Cmd.none)
 
 
 
@@ -48,7 +55,11 @@ update msg model =
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Time.every second Tick
+  case model.running of
+    True ->
+      Time.every second Tick
+    False ->
+      Sub.none
 
 
 
@@ -59,15 +70,21 @@ view : Model -> Html Msg
 view model =
   let
     angle =
-      turns (Time.inMinutes model)
+      turns (Time.inMinutes model.time)
 
     handX =
       toString (50 + 40 * cos angle)
 
     handY =
       toString (50 + 40 * sin angle)
+
+    buttonText =
+      if model.running then "Stop" else "Start"
   in
-    svg [ viewBox "0 0 100 100", width "300px" ]
+    div []
+    [ svg [ viewBox "0 0 100 100", width "300px" ]
       [ circle [ cx "50", cy "50", r "45", fill "#0B79CE" ] []
       , line [ x1 "50", y1 "50", x2 handX, y2 handY, stroke "#023963" ] []
       ]
+      , button [onClick ToggleRunning] [text buttonText]
+    ]
