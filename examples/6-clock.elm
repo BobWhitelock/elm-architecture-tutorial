525bd134fb5f6271fc64cb7109a878709608607c
Bob Whitelock <bob.whitelock1@gmail.com>
2016-09-12 22:55:03 +0100

    WIP

--- examples/6-clock.elm
+++ examples/6-clock.elm
@@ -69,22 +69,29 @@ subscriptions model =
 view : Model -> Html Msg
 view model =
   let
-    angle =
-      turns (Time.inMinutes model.time)
-
-    handX =
-      toString (50 + 40 * cos angle)
-
-    handY =
-      toString (50 + 40 * sin angle)
-
     buttonText =
       if model.running then "Stop" else "Start"
   in
     div []
     [ svg [ viewBox "0 0 100 100", width "300px" ]
       [ circle [ cx "50", cy "50", r "45", fill "#0B79CE" ] []
-      , line [ x1 "50", y1 "50", x2 handX, y2 handY, stroke "#023963" ] []
+      , clockHand model.time Time.inSeconds 40
+      , clockHand model.time Time.inMinutes 30
+      , clockHand model.time Time.inHours 20
       ]
       , button [onClick ToggleRunning] [text buttonText]
     ]
+
+clockHand : Time -> (Time -> Float) -> Float -> Html Msg
+clockHand time inUnits length =
+  let
+    angle =
+      turns (inUnits time)
+
+    handX =
+      toString (50 + length * cos angle)
+
+    handY =
+      toString (50 + length * sin angle)
+  in
+      line [ x1 "50", y1 "50", x2 handX, y2 handY, stroke "#023963" ] []
